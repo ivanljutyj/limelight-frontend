c6dcdbdd8afacce3cf05133bade3f6dcc51c3a65
Ivan Ivanoff <contact@ivanon.io>
2020-06-13 07:59:38 -0400

    change image animation

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="content">
     <nuxt-link :to="url">
-      <img :src="cover"/>
+      <img id="cover" :src="cover"/>
       <nuxt-link :to="url" class="button">View Release</nuxt-link>
     </nuxt-link>
   </div>
@@ -30,8 +30,11 @@ export default {
       this.release = response[response.length - 1];
       this.url = '/releases/' + this.release.slug
       this.cover = this.$axios.defaults.baseURL + this.release.cover.url;
-      this.timeline.to('.content img', 0.5, { opacity: 1, rotationY: 0 })
-      this.timeline.staggerTo('.content .button', 0.5, { opacity: 1 }, 0.3)
+      let cover = document.getElementById('cover');
+      cover.onload = function () {
+        this.timeline.to('.content img', 0.5, { opacity: 1, rotationY: 0 })
+        this.timeline.staggerTo('.content .button', 0.5, { opacity: 1 }, 0.3)
+      }
     })
   },
 }
