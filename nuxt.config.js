66dff2a0244414b41f1f82bd63915a1ff6a62c2c
Ivan Ivanoff <contact@ivanon.io>
2020-08-01 10:56:30 -0400

    fix bug

--- nuxt.config.js
+++ nuxt.config.js
@@ -73,7 +73,7 @@ export default {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
-    baseURL: 'http://api.limelightvisions.com'
+    baseURL: 'https://api.limelightvisions.com'
   },
   /*
   ** Style resources
