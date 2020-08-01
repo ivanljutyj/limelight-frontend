6b989cf302f0627a437975fdcb3802a1e9a5502f
Ivan Ivanoff <contact@ivanon.io>
2020-08-01 02:53:11 -0400

    fix bugs

--- nuxt.config.js
+++ nuxt.config.js
@@ -95,12 +95,12 @@ export default {
   },
   generate: {
     routes() {
-      let releases = axios.get('https://api.limelightvisions.com/releases', {params: {size: 10}}).then((res) => {
+      let releases = axios.get('https://api.limelightvisions.com/releases').then((res) => {
         return res.data.releases.map((release) => {
           return '/releases/' + release.slug
         })
       })
-      let artists = axios.get('https://api.limelightvisions.com/artists', {params: {size: 10}}).then((res) => {
+      let artists = axios.get('https://api.limelightvisions.com/artists').then((res) => {
         return res.data.artists.map((artist) => {
           return '/artists/' + artist.slug
         })
